b79c71cccd40d1b6dc7f8d8aee478f84587e14f0
Mia Peace <mrssilentwar@gmail.com>
2020-12-06 22:08:26 -0800

    fixed minor error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import Icon from '@material-ui/core/Icon';
 import './App.css';
 
 class App extends Component {
