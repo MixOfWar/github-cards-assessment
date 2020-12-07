41e8481a141c7952a0ce81ac50ff2e11ad139dba
Mia Peace <mrssilentwar@gmail.com>
2020-12-06 22:03:56 -0800

    added ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import Icon from '@material-ui/core/Icon';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: {},
+    active: false,
+  }
+
+  handleToggle = () => {
+    this.setState((state) => ({active: !state.active}))
+  }
+
+  componentDidMount() {
+    const githubURL = 'https://api.github.com/users/mrssilentwar';
+    fetch(githubURL).then((data) => data.json()).then(result => {
+      this.setState({user: result})
+    })
+  }
+
+  render() {
+    const {user, active} = this.state;
+    return (
+      <div className="App">
+        <header>
+          <Button variant="contained" color="secondary" onClick={this.handleToggle}>Click Me!</Button>
+        </header>
+        {active && 
+          <a href={user.html_url}>
+            <Card className='user_tile'>
+              <img alt={user.login} src={user.avatar_url} />
+            <p className="user_title">{user.login}</p>
+            <p>{user.name}</p> 
+            <p>{user.bio}</p>
+          </Card>
+          </a>
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
